a295446665ab479379b941d3155de121650d1ff6
Manu <manuherrera8@gmail.com>
2023-04-11 12:22:56 -0300

    Update macOS version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "ViewStateController",
     platforms: [
         .iOS(.v16),
-        .macOS(.v12)
+        .macOS(.v13)
     ],
     products: [
         .library(
